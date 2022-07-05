2712901768db8d6d4effdc684322c89dbb9489ba
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-07-05 16:46:08 +0100

    create theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import Navbar from "./components/Navbar";
+
+const theme = {
+  colors: {
+    header: "#ffffff",
+    navItemsColor: "#2C3539",
+    body: "#ffffff",
+    productDesc: "#5C1A8D",
+    productLeft: "#A792FE",
+    productRight: "#E5E5E5",
+    signUpBtn: "#2C3539",
+    signUp: "#F2C94C",
+    footer: "#DEB63D",
+  },
+};
 
 const App = () => {
   return (
-    <>
-    <Navbar />
-    </>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <>
+        <Navbar />
+      </>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
