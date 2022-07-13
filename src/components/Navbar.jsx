0d900b43ab74b35e2ff1d179c22986cf5ee3fb84
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-07-13 07:56:14 +0100

    fix mobile responsiveness and navigation for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import {
-  NavbarWrapper,
   NavbarContainer,
   NavList,
   NavListItem,
+  Menu,
 } from "./styles/Navbar.styled";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const openMenu = () => setOpen(!open);
+
   return (
-    <NavbarWrapper>
-      <NavbarContainer>
-        <Logo />
-        <NavList>
-          <NavListItem>The App</NavListItem>
-          <NavListItem>About Us</NavListItem>
-          <NavListItem>Get in Touch</NavListItem>
-        </NavList>
-      </NavbarContainer>
-    </NavbarWrapper>
+    <NavbarContainer>
+      <Logo />
+      <NavList open={open}>
+        <NavListItem>The App</NavListItem>
+        <NavListItem>About Us</NavListItem>
+        <NavListItem>Get in Touch</NavListItem>
+      </NavList>
+      <Menu open={open} onClick={openMenu}>
+        <div />
+        <div />
+        <div />
+      </Menu>
+    </NavbarContainer>
   );
 };
 
--- src/components/styles/Navbar.styled.jsx
+++ src/components/styles/Navbar.styled.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-export const NavbarWrapper = styled.div``;
 export const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
@@ -8,10 +7,34 @@ export const NavbarContainer = styled.div`
   padding: 0 70px;
   margin-top: 2rem;
   height: 50px;
+  @media (max-width: ${({ theme }) => theme.mobile.xs}px) {
+    padding: 0 25px;
+  }
 `;
 export const NavList = styled.ul`
   display: flex;
   align-items: center;
+  @media (max-width: ${({ theme }) => theme.mobile.md}px) {
+    padding: 0 120px;
+    display: none;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    background: #ffffff;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+    height: 60vh;
+    width: 100%;
+    text-align: left;
+    margin-top: 3.5rem;
+    position: absolute;
+    top: 30px;
+    left: 0;
+    z-index: 10;
+    transition: all 1s ease;
+  }
+  @media (max-width: ${({ theme }) => theme.mobile.xs}px) {
+    padding: 0 70px;
+    }
 `;
 export const NavListItem = styled.li`
   margin-right: 1.5rem;
@@ -21,6 +44,36 @@ export const NavListItem = styled.li`
   color: ${({ theme }) => theme.colors.navItemsColor};
   cursor: pointer;
   @media (max-width: ${({ theme }) => theme.mobile.md}px) {
-    display: none;
+    margin-bottom: 1rem;
+  }
+`;
+export const Menu = styled.div`
+  display: none;
+  @media (max-width: ${({ theme }) => theme.mobile.md}px) {
+    cursor: pointer;
+    display: block;
+    padding: 8px;
+    div {
+      width: 35px;
+      height: 5px;
+      background-color: #232c35;
+      margin: 6px 0;
+      transition: 0.5s;
+      :first-child {
+        transform: ${({ open }) =>
+          open
+            ? "rotate(-45deg) translate(-9px, 6px)"
+            : "rotate(0) translate(0)"};
+      }
+      :nth-child(2) {
+        opacity: ${({ open }) => (open ? "0" : "1")};
+      }
+      :nth-child(3) {
+        transform: ${({ open }) =>
+          open
+            ? "rotate(45deg) translate(-8px, -8px)"
+            : "rotate(0) translate(0)"};
+      }
+    }
   }
 `;
